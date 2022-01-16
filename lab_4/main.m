fdcfab7b3f358069eda0caf151d493f858c13f17
josemiguelbento <jose.miguel.bento@gmail.com>
2022-01-16 22:29:50 +0000

    lab 4 feito i guess

--- lab_4/main.m
+++ lab_4/main.m
@@ -23,171 +23,122 @@ u_0 = omega_0;
 
 dZr = 10; %m
 
-%% Root locus for PID open loop
+fig_count = 1;
+%% 4.2 Root locus for PID open loop different pairs of zeros
 % Transfer Function for proportional
 s = tf('s');
 
-z1 = [1,20,100];
-z2 = [2,3,4];
+z1_test = [0.5, 1,2];
+z2_test = [1,10,20];
 
 
 
 % For K > 0 the root locus is
-for i = 1:length(z1)
-    figure(i)
-    g_pid = (s-z1(i))*(s-z2(i))/(s^3*(s+300));
-    rlocus(g_pid);
-    title(strcat("Root locus PID open loop", "z_1 = ", num2str(z1(i))))
+for i = 1:length(z1_test)
+    figure(fig_count)
+    fig_count = fig_count+1;
+    g_pid_ol_test = (s+z1_test(i))*(s+z2_test(i))/(s^3*(s+300));
+    rlocus(g_pid_ol_test);
+    title(strcat("Root locus PID", " z_1 = ", num2str(z1_test(i)), "  z_2 = ", num2str(z2_test(i))))
 end
 
 
-% % For K < 0 we do rlocus for the symetric of the transfer function
-% figure(2)
-% rlocus(-g_prop);
-% title('Root locus for K < 0, proportional loop')
+%% 4.3 z_1=1 z_2=10 finding K so poles are -1, -21 x 2, -224
+z1 = 1;
+z2 = 10;
+g_pid_ol = (s+z1)*(s+z2)/(s^3*(s+300));
+figure(fig_count)
+fig_count = fig_count+1;
+rlocus(g_pid_ol);
+[r_procura,k_procura] = rlocus(g_pid_ol);
+title('Root locus for PID z_1 = 1 z_2 = 10')
+for j = 1:length(k_procura)
+    if(abs(r_procura(1,j) - (-1))< 0.01 && ...
+            imag(r_procura(2,j)) < 0.0001 && ...
+            abs(r_procura(3,j) - r_procura(2,j)) < 0.0001 && ...
+            imag(r_procura(4,j)) < 0.0001)
+        k_alvo = k_procura(j);
+        polo_alvo = r_procura(1,j);
+        break
+    end
+end
+
+% Calculating coefficients from the K found
+kd_procura = k_alvo * M/600/Kt/omega_0;
+kp_procura = kd_procura*(z1+z2);
+ki_procura = kd_procura*z1*z2;
+
+%% Running the complete simulation for the data from question 4.3
+Kd = kd_procura;
+Kp = kp_procura;
+Ki = ki_procura;
+
+simout_tot = sim('total_lab4','StopTime',num2str(finaltime),'FixedStep',num2str(StepSize));
+
+% Plotting the step response of the closed-loop linear system with the
+% pid controller.
+figure(fig_count)
+fig_count = fig_count+1;
+plot(simout_tot.get('z_pid').time, simout_tot.get('z_pid').signals.values);
+xlabel('time (s)')
+ylabel('z (m)')
+title({strcat("Altitude ", "dZr = ", num2str(dZr), " m"),...
+    strcat('z_1 = ', num2str(z1), 'z_2 = ', num2str(z2), '   K_p = ',...
+    num2str(Kp), '   K_i = ', num2str(Ki), '   K_d = ', num2str(Kd))})
+legend('PID','Location','southeast');
 
 
-% %% Root locus for derivative loop
-% % Transfer Function for Proportional and Derivative
-% z_test = [-20, -10, 10, 20];
-% 
-% % For K > 0 we do rlocus for several values of z
-% for i = 1:length(z_test)
-%     figure(i+2)
-%     g_prop_der = (s+z_test(i))/(s^2*(s+300));
-%     rlocus(g_prop_der);
-%     title(strcat("Root locus ", "z = ", num2str(z_test(i))))
-% end
-% %% Quastion 3.8
-% % Procurar o K que satisfaz a condição de haver um polo duplo
-% z_procura = 1;
-% polo_procurado = -2.01;
-% g_procura = (s+z_procura)/(s^2*(s+300));
-% [r_procura,k_procura] = rlocus(g_procura);
-% k_alvo = 0;
-% polo_alvo = 0+0i;
-% for j = 2:length(k_procura)
-%     if(abs(r_procura(1,j)-r_procura(2,j))<0.0001)
-%         k_alvo = k_procura(j);
-%         polo_alvo = r_procura(1,j);
-%         break
-%     end
-% end
-% 
-% % Calculating Kp and Kd from the K found in the search for the double pole
-% % (-2.01). The K for the pole mentioned is K = 1192.
-% 
-% Kd_procura = k_alvo/(600*Kt*omega_0/M);
-% Kp_procura = z_procura * Kd_procura;
-% 
-% 
-% 
-% %% Running the complete simulation for the data from question 3.8
-% % For this set of data, we exemplify the difference in response of the
-% % systems analysed.
-% 
-% Kp = Kd_procura;
-% Kd = Kp_procura;
-% 
-% 
-% K_prop = 600*Kp*Kt*omega_0/M;
-% K_prop_der = 600*Kd*Kt*omega_0/M;
-% z = Kp/Kd;
-% 
-% % Plotting the root locus for z = 1 and displaying the double pole
-% figure(3+length(z_test))
-% g_prop_der = (s+z)/(s^2*(s+300));
-% rlocus(g_prop_der);
-% hold on
-% plot(real(r_procura(1,j)), imag(r_procura(1,j)), 'r.','MarkerSize', 15);
-% title('Root locus for z = 1, displaying the double pole');
-% 
-% % Running the simulation for the previous data
-% simout_tot = sim('total_lab3','StopTime',num2str(finaltime),'FixedStep',num2str(StepSize));
-% 
-% % Plotting the step response of the closed-loop linear system with the
-% % proportional-derivative controller.
-% figure(4+length(z_test))
-% plot(simout_tot.get('z_p').time, simout_tot.get('z_p').signals.values);
-% hold on
-% plot(simout_tot.get('z_pd').time, simout_tot.get('z_pd').signals.values);
-% plot(simout_tot.get('z_pdtf').time, simout_tot.get('z_pdtf').signals.values);
-% xlabel('time (s)')
-% ylabel('z (m)')
-% title({strcat("Altitude ", "dZr = ", num2str(dZr), " m"),...
-%     strcat('z = ', num2str(z), '   K_p = ', num2str(Kp), '   K_d = ', num2str(Kd))})
-% legend('prop','dif prop','dif prop with tf','Location','southeast');
-% 
-% % Here we plotted 3 different graphs to compare different controllers.
-% %
-% % As indicated in the legend, the blue one is originated in a simple
-% % proportional controller. We can see it doesnt stabilize in the reference
-% % value.
-% % 
-% % The red graph is the step response of a proportional derivative
-% % controller, the simulation for which is done step by step using the
-% % script from lab class 2. (in simulink it is the "proportional
-% % derivative") block. This is the answer to question 3.8
-% %
-% % The final yellow graph is the step response of a proportional derivative
-% % controller, the simulation for which is done with a single transfer
-% % function (in simulink it is the "tf proportional derivative" block).
-% % Note that this one differs from the second graph although theoretically
-% % they should look alike.
-% %% Effect of a varying z for contant K 3.9a
-% % We now plot the step response keeping K constant but varying z.
-% K_39a = 1192;
-% z_39a = [1];
-% 
-% legendcella = {};
-% 
-% for i = 1:length(z_39a)
-%     z = z_39a(i);
-%     Kd = K_39a/(600*Kt*omega_0/M);
-%     Kp = z_39a * Kd;
-%     K_prop = 600*Kp*Kt*omega_0/M;
-%     K_prop_der = 600*Kd*Kt*omega_0/M;
-%     simout_tot = sim('total_lab3','StopTime',num2str(finaltime),'FixedStep',num2str(StepSize));
-%     
-%     figure(5+length(z_test))
-%     %plot(simout_tot.get('z_p').time, simout_tot.get('z_p').signals.values);
-%     plot(simout_tot.get('z_pd').time, simout_tot.get('z_pd').signals.values);
-%     %plot(simout_tot.get('z_pdtf').time, simout_tot.get('z_pdtf').signals.values);
-%     hold on
-%     xlabel('time (s)')
-%     ylabel('z (m)')
-%     title(strcat("Altitude ", "dZr = ", num2str(dZr), " m    For constant K = 1192"))
-%     legendcella = [legendcella, cellstr(strcat('z = ', num2str(z)))];
-% end
-% legend(legendcella,'Location','southeast');
-% % We can see 
-% 
-% %% Effect of a varying K for contant z 3.9b
-% finaltime = 10;
-% 
-% K_39b = [150,500, 1192, 3000];
-% z_39b = 1;
-% 
-% legendcellb = {};
-% 
-% for i = 1:length(K_39b)
-%     z = z_39b;
-%     Kd = K_39b(i)/(600*Kt*omega_0/M);
-%     Kp = z_39b * Kd;
-%     K_prop = 600*Kp*Kt*omega_0/M;
-%     K_prop_der = 600*Kd*Kt*omega_0/M;
-%     simout_tot = sim('total_lab3','StopTime',num2str(finaltime),'FixedStep',num2str(StepSize));
-%     
-%     figure(6+length(z_test))
-%     %plot(simout_tot.get('z_p').time, simout_tot.get('z_p').signals.values);
-%     plot(simout_tot.get('z_pd').time, simout_tot.get('z_pd').signals.values);
-%     %plot(simout_tot.get('z_pdtf').time, simout_tot.get('z_pdtf').signals.values);
-%     hold on
-%     xlabel('time (s)')
-%     ylabel('z (m)')
-%     title(strcat("Altitude ", "dZr = ", num2str(dZr), " m   For constant z = 1"))
-%     legendcellb = [legendcellb, cellstr(strcat('K = ', num2str(K_39b(i))))];
-% end
-% legend(legendcellb,'Location','southeast');
-% 
-% 
+%% Effect of a varying z1 and z2 for contant K 4.4a
+finaltime = 10;
+
+K_44a = 11740;
+z1_44a = [0.5, 1,2];
+z2_44a = [1, 10,20];
+
+legendcella = {};
+
+for i = 1:length(z1_44a)
+    Kd = K_44a * M/600/Kt/omega_0;
+    Kp = Kd*(z1_44a(i)+z2_44a(i));
+    Ki = Kd*z1_44a(i)*z2_44a(i);
+    simout_tot = sim('total_lab4','StopTime',num2str(finaltime),'FixedStep',num2str(StepSize));
+    
+    figure(fig_count)
+
+    plot(simout_tot.get('z_pid').time, simout_tot.get('z_pid').signals.values);
+    hold on
+    xlabel('time (s)')
+    ylabel('z (m)')
+    title(strcat("Altitude ", "dZr = ", num2str(dZr), " m   For constant K = 11740"))
+    legendcella = [legendcella, cellstr(strcat( "z_1 = ", num2str(z1_44a(i)), "  z_2 = ", num2str(z2_44a(i))))];
+end
+fig_count = fig_count+1;
+legend(legendcella,'Location','southeast');
+
+
+%% Effect of a varying K for contant z1 and z2 4.4b
+finaltime = 10;
+
+K_44b = [150,500, 11740, 20000];
+z1_44b = z1;
+z2_44b = z2;
+
+legendcellb = {};
+
+for i = 1:length(K_44b)
+    Kd = K_44b(i) * M/600/Kt/omega_0;
+    Kp = Kd*(z1_44b+z2_44b);
+    Ki = Kd*z1_44b*z2_44b;
+    simout_tot = sim('total_lab4','StopTime',num2str(finaltime),'FixedStep',num2str(StepSize));
+    
+    figure(fig_count)
+
+    plot(simout_tot.get('z_pid').time, simout_tot.get('z_pid').signals.values);
+    hold on
+    xlabel('time (s)')
+    ylabel('z (m)')
+    title(strcat("Altitude ", "dZr = ", num2str(dZr), " m   For constant z_1 = 1, z_2 = 10"))
+    legendcellb = [legendcellb, cellstr(strcat('K = ', num2str(K_44b(i))))];
+end
+fig_count = fig_count+1;
+legend(legendcellb,'Location','southeast');
