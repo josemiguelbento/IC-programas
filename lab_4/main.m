ddefa5c67414c3ad72457a71dd4f7ae05ae03679
josemiguelbento <jose.miguel.bento@gmail.com>
2022-01-21 14:54:31 +0000

    alguns comentarios feitos para lab 4 falta comentar zeros positivos

--- lab_4/main.m
+++ lab_4/main.m
@@ -21,15 +21,18 @@ Z0 = 2; %m
 omega_0 = sqrt(G*M/Kt); %rad/s
 u_0 = omega_0;
 
-dZr = 10; %m
+dZr = 1; %m
 
 fig_count = 1;
 %% 4.2 Root locus for PID open loop different pairs of zeros
 % Transfer Function for proportional
 s = tf('s');
 
-z1_test = [0.5, 1,2];
-z2_test = [1,10,20];
+% Because these values enter the gain expression as (s+z), for a positive
+% value here, we have a zero on the left complex semiplane.
+
+z1_test = [0.5, 1, 2];
+z2_test = [1, 10, 20];
 
 
 
@@ -42,6 +45,54 @@ for i = 1:length(z1_test)
     title(strcat("Root locus PID", " z_1 = ", num2str(z1_test(i)), "  z_2 = ", num2str(z2_test(i))))
 end
 
+%%
+% In all the previous plots we can see the movement of 4 poles. For K=0, 3
+% of them sit on the origin (Re = 0, Im = 0), and the other one is equal 
+% to -300. If we zoom in near the origin we can see that at first, 2 of the
+% poles that sit at the origin will go to the right complex semiplane. For
+% example, for the zeros at z_1 = 1 and z_2 = 10, we see that the two poles
+% in question are on the right complex semiplane when K is lower than 275.
+% Therefore, the system is unstable for K lower than 275 (for these zeros).
+% For K = 275 (or somewhere close to it), the system is marginally stable,
+% since we have 2 poles with 0 real part and 2 poles on the left complex
+% semiplane. For K greater than 275, we get the 4 poles on the left complex
+% semiplane, thus concluding that the system is stable. This is illustrated
+% in the step response in the last section.
+
+%% 
+% We will now analyse the root-locus for positive zeros (on the right
+% complex semiplane).
+
+% Because these values enter the gain expression as (s+z), for a negative
+% value here, we have a zero on the right complex semiplane.
+
+z1_test = [-1, -5];
+z2_test = [-2, -8];
+
+
+
+% For K > 0 the root locus is
+for i = 1:length(z1_test)
+    figure(fig_count)
+    fig_count = fig_count+1;
+    g_pid_ol_test = (s+z1_test(i))*(s+z2_test(i))/(s^3*(s+300));
+    rlocus(g_pid_ol_test);
+    title(strcat("Root locus PID", " z_1 = ", num2str(z1_test(i)), "  z_2 = ", num2str(z2_test(i))))
+end
+
+%%
+% In all the previous plots we can see the movement of 4 poles. For K=0, 3
+% of them sit on the origin (Re = 0, Im = 0), and the other one is equal 
+% to -300. If we zoom in near the origin we can see that at first, 2 of the
+% poles that sit at the origin will go to the right complex semiplane. For
+% example, for the zeros at z_1 = 1 and z_2 = 10, we see that the two poles
+% in question are on the right complex semiplane when K is lower than 275.
+% Therefore, the system is unstable for K lower than 275 (for these zeros).
+% For K = 275 (or somewhere close to it), the system is marginally stable,
+% since we have 2 poles with 0 real part and 2 poles on the left complex
+% semiplane. For K greater than 275, we get the 4 poles on the left complex
+% semiplane, thus concluding that the system is stable. This is illustrated
+% in the step response in the last section.
 
 %% 4.3 z_1=1 z_2=10 finding K so poles are -1, -21 x 2, -224
 z1 = 1;
@@ -89,7 +140,7 @@ legend('PID','Location','southeast');
 
 
 %% Effect of a varying z1 and z2 for contant K 4.4a
-finaltime = 10;
+finaltime = 8;
 
 K_44a = 11740;
 z1_44a = [0.5, 1,2];
@@ -117,9 +168,9 @@ legend(legendcella,'Location','southeast');
 
 
 %% Effect of a varying K for contant z1 and z2 4.4b
-finaltime = 10;
+finaltime = 5;
 
-K_44b = [150,500, 11740, 20000];
+K_44b = [150, 275, 1000, 11740];
 z1_44b = z1;
 z2_44b = z2;
 
